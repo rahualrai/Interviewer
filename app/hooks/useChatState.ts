d0628f0af54d9183ae0c6541cb56b8603212fd14
Rahual Rai <rairahual7@gmail.com>
2024-11-08 02:59:09 -0500

    Updated prompt.

--- app/hooks/useChatState.ts
+++ app/hooks/useChatState.ts
@@ -11,6 +11,7 @@ type FileDetail = {
 
 export const useChatState = () => {
   
+  const [initialThreadMessage, setInitialThreadMessage] = useState('Introduce yourself and ask for the participants name and age.');
   
   const [assistantName, setAssistantName] = useState('');
   const [assistantModel, setAssistantModel] = useState('gpt-3.5-turbo-1106');
@@ -23,7 +24,6 @@ export const useChatState = () => {
   const [threadId, setThreadId] = useState<string | null>(null);
   const [isStartLoading, setStartLoading] = useState(false);
   const [isSending, setIsSending] = useState(false);
-  const [initialThreadMessage, setInitialThreadMessage] = useState('You are a Pirate! introduce yourself');
   const [statusMessage, setStatusMessage] = useState('');
   const counter = useRef(0);
   const inputRef = useRef(null);
